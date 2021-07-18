39f189e2079d142ca8c276367adad38b46c0cb31
sydney <sydneychase5841@gmail.com>
2021-07-18 07:34:42 -0400

    Assignment started

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
+class Song
+    attr_reader :artist, :genre
 
+
+@@all = []
+
+def initialize(name, artist, genre)
+    @name = name
+    @artist = artiist
+    @genre = genre
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+end
